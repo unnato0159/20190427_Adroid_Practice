3f908529e9253159537e27600aa408a8ae317b73
unnato0159 <unnato01@naver.com>
2019-04-27 15:43:51 +0900

    getVIew 와 getDropdownView 의 모양을 서로다르게

--- app/src/main/java/com/tje/a20190427_adroid_practice/adapters/PizzaStoreAdapter.java
+++ app/src/main/java/com/tje/a20190427_adroid_practice/adapters/PizzaStoreAdapter.java
@@ -33,17 +33,15 @@ public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
        View row = convertView;
 
        if(row == null){
-           row=inf.inflate(R.layout.pizza_spinner_list_item,null);
+           row=inf.inflate(R.layout.pizza_spinner_item,null);
        }
        PizzaStore storeData = mList.get(position);
        ImageView logoImgView = row.findViewById(R.id.logoImgView);
        TextView nameTxt = row.findViewById(R.id.nameTxt);
-       TextView locationTxt = row.findViewById(R.id.locationTxt);
-       TextView openTimeTxt = row.findViewById(R.id.openTimeTxt);
+
 
        nameTxt.setText(storeData.storeName);
-       locationTxt.setText(String.format("(%s)",storeData.location));
-       openTimeTxt.setText(storeData.openTime);
+
 
 
 
